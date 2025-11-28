247f3f43e0d3b0ac972636143185ad8439f35b1b
Yasir Hussain <yasirbasumo@gmail.com>
2025-11-28 15:20:07 +0500

    Add files via upload

--- main04.py
+++ main04.py
@@ -1,6 +1,42 @@
-sentence=input("Enter any sentence: ")
-new=sentence.lower()
-if new[0:5]=='namal':
-    print("yes sting with namal")
-else:
-    print("Stirng does not start with namal")
+def even(n):
+    if n%2==0:
+        return "Even Number"
+    else:
+        return "Odd number"
+def prime(n):
+    if n < 2:
+        print("Not prime")
+        return False
+    for i in range(2, n):
+        if n % i == 0:
+            print("Not prime")
+            return False  
+    print("Prime")
+    return True
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
+while True:
+    print("welcome to New calculator")
+    print("1. Even/ Odd Numbber")
+    print("2. Check Prime")
+    print("3. Factorial")
+    print("4. Exit")
+
+    user=int(input("Choose anyone option: "))
+    if user==4:
+        break
+    num=int(input("Enter any number:"))
+    if user==1:
+        result=even(num)
+        print(result)
+    elif user==2:
+        result=prime(num)
+        print(result)
+    elif user==3:
+        result=factorial(num)
+        print(result)
+    else:
+        print("invalid syntax")
