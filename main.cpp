5299e1a6d61563c0123de9597271e0492c32e4f7
Yasir Hussain <yasirbasumo@gmail.com>
2025-11-30 23:01:43 +0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,27 +1,21 @@
 #include <iostream>
+
 using namespace std;
 
-int main() {
-    int first=0,sec=1,next,n;
-    cout<<"Enter hoe many term you would like to print"<<endl;
-    cin>>n;
-    cout<<"fibonacci Sequence: ";
-    for(int i=0;i<n;i++){
-        if(i==0){
-            cout<<first<<"  ";
-        }
-        else if(i==1){
-            cout<<sec<<"  ";
+int main()
+{
+    int i=1;
+    for(int i=1;i<101;i++)
+        if(i%5==0)
+    {
+        cout<<i<<" : firrrzz"<<endl;
+    }
+        else if(i%3==0){
+            cout<<i<<" : buzz"<<endl;
         }
         else{
-            next=first+sec;
-            cout<<next<<"  ";
-            first=sec;
-            sec=next;
+            cout<<i<<endl ;
         }
 
-    }
-
     return 0;
-
 }
