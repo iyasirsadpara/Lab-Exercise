615d4c273bf3e2e54e9afb1ad12e42a3d2be9d5f
Yasir Hussain <yasirbasumo@gmail.com>
2025-12-11 21:35:52 +0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,32 +1,23 @@
 #include <iostream>
-#include <vector>
-#include <set>
+#include <string>
+
 using namespace std;
-
-vector<int> find_common_elements(const vector<int>& arr1, const vector<int>& arr2) {
-    set<int> set1(arr1.begin(), arr1.end());
-    set<int> set2(arr2.begin(), arr2.end());
-    vector<int> common_elements;
-
-    for (int num : set1) {
-        if (set2.find(num) != set2.end()) {
-            common_elements.push_back(num);
+int counter(const string&input){
+    int con=0;
+    for(char ch : input){
+        if(ch==' '){
+            con=con+1;
         }
     }
-    return common_elements;
+    return con+1;//Add last word which has no space
 }
 
-int main() {
-    vector<int> array1 = {1, 2, 3, 4, 5};
-    vector<int> array2 = {4, 5, 6, 7, 8};
-
-    vector<int> common_elements = find_common_elements(array1, array2);
-
-    cout << "Common elements: ";
-    for (int num : common_elements) {
-        cout << num << " ";
-    }
-    cout << endl;
+int main(){
+    string input;
+    cout << "ENTER YOUR SENTENCE:\n" << endl;
+    getline(cin,input);
+    int counted=counter(input);
+    cout << "There are "<<counted<<" words in sentence\n" << endl;
 
     return 0;
 }
