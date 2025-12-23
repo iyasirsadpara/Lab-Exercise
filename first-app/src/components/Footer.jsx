84b5d1e02ee404000ea5ea74e76d58187f7da1b3
Yasir Hussain <yasirbasumo@gmail.com>
2025-12-23 10:03:20 +0500

    Initial commit

---
+++ first-app/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Footer.css"
+const Footer = () => {
+  return (
+    <div>
+      <div className="footer">
+        Copyright &copy; code with harry | All rights reserved
+      </div>
+    </div>
+  )
+}
+
+export default Footer
