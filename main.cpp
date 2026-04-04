d7b5d0ab251f44b8b60cda3dab6cd5dcafdee023
Yasir Hussain <yasirbasumo@gmail.com>
2026-04-04 23:42:31 +0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,38 +1,28 @@
-//Write a function to find if there exists a pair whose sum is equal to the given target. You may need to consider edge cases where there are no valid pairs.
+// Write a function that converts all uppercase letters in a string to lowercase, and all lowercase letters to uppercase.
 #include <iostream>
-#include <vector>
+#include <string>
 
 using namespace std;
-bool sum(const vector<int>& arr, int target) {
-    int n = arr.size();
-    for (int i = 0; i < n; i++) {
-        for (int j = i + 1; j < n; j++) {
-            if (arr[i] + arr[j] == target) {
-                return true;
-            }
+string myfun(string&input){
+    string result=input;
+    for(char&ch:result){
+        if(isupper(ch)){
+            ch=tolower(ch);
+        }
+        else if(islower(ch)){
+            ch=toupper(ch);
         }
     }
-
-    return false;
+    return result;
 }
 
-int main() {
-    int n, target;
-    cout << "Enter the size of the array: ";
-    cin >> n;
-    vector<int> arr(n);
-    for (int i = 0; i < n; i++) {
-        cout<<"Enter elements at ["<<i+1<<"] =";
-        cin >>arr[i];
-    }
-
-    cout << "Enter the target sum: ";
-    cin >> target;
-    if (sum(arr, target)) {
-        cout << "There exists a pair whose sum is =" << target << endl;
-    } else {
-        cout << "No such pair exists." << endl;
-    }
+int main()
+{
+    string input,result;
+    cout << "Enter any text:\n";
+    getline(cin,input);
+    result=myfun(input);
+    cout<<"convert text:\n"<<result;
 
     return 0;
 }
