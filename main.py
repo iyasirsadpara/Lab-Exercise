0385463791f2a2775efe8b7d29d0015fdd2310fd
Yasir Hussain <yasirbasumo@gmail.com>
2026-04-07 21:44:08 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
-user=input("Enter any string: ")
-length=len(user)
-print("The length of the given string is:",length)
+import random
+print("welcome to r p s game")
+
+choices=['rock','paper','scissors']
+while True:
+    user=input("enter your choice:")
+    computer=random.choice(choices)
+    print("computer chose",computer)
+    if user==computer:
+        print("tie")
+    elif user=='rock' and computer=='paper'or user=='paper' and computer=='scissors'or user=='scissors' and computer=='rock':
+        print("you loose")
+    elif user=='paper' and computer=='rock' or user=='scissors' and computer=='paper'or user=='rock' and computer=='scissors':
+        print("you win")
+    else:
+        print("invalid choice")
