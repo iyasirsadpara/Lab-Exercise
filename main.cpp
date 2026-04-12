911bf9b995397549d2aa21a08a8db6e3b7371a30
Yasir Hussain <yasirbasumo@gmail.com>
2026-04-12 23:05:38 +0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,17 +1,27 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-    lon g long num,counter;
-    cout<<"Enter any numbers:"<<endl;
-    cin>>num;
-    counter=0;
-    while(num>0)
-    {
-        num=num/10;
-        counter=counter+1;
+int main() {
+    int first=0,sec=1,next,n;
+    cout<<"Enter hoe many term you would like to print"<<endl;
+    cin>>n;
+    cout<<"fibonacci Sequence: ";
+    for(int i=0;i<n;i++){
+        if(i==0){
+            cout<<first<<"  ";
+        }
+        else if(i==1){
+            cout<<sec<<"  ";
+        }
+        else{
+            next=first+sec;
+            cout<<next<<"  ";
+            first=sec;
+            sec=next;
+        }
+
     }
-    cout<<"The digits in you enter number are: "<<counter;
+
     return 0;
+
 }
